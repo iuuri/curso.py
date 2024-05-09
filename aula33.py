c74a225377e3436f3df8db9cd96ce467ef1ec84b
Iuri Souza <iuri.souzasantos@gmail.com>
2024-05-09 15:21:37 -0300

    novas aulas

---
+++ aula33.py
@@ -0,0 +1,7 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+
+string = 'Iuri Souza'
+outra_variavel = string
---
+++ aula34.py
@@ -0,0 +1,6 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
